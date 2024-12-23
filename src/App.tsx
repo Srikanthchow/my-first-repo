6b48e6e6cf3ee01985a21b444bf8407db3a9aab5
Srikanthchow <kilarisrikanthchowdary@gmail.com>
2024-12-23 09:32:01 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+//import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import About from './components/About'
+import Choose from './components/Choose'
+import Add from './components/Add'
+import Menu from './components/Menu'
+import BookTable from './components/BookTable'
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      <NavBar />
+      <Home />
+      <About />
+      <Choose />
+      <Add />
+      <Menu />
+      <BookTable />
+
+
+    
+    </div>
   )
 }
-
-export default App
